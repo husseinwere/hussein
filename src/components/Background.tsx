f9353b2524e73a6515ab218b0dff78684d894a93
Hussein Were <hussein.were@yahoo.com>
2024-04-17 23:18:16 +0300

    background animation

---
+++ src/components/Background.tsx
@@ -0,0 +1,49 @@
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import blur from '../assets/images/blur.webp'
+import paint from '../assets/images/paint.jpg'
+import { TextureLoader } from "three";
+import { Suspense, useContext, useRef } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
+
+function Scene() {
+  const { mode } = useContext(ThemeContext)
+  const paintMap = useLoader(TextureLoader, paint.src)
+  const blurMap = useLoader(TextureLoader, blur.src)
+  const sphereGroup: any = useRef()
+  useFrame(({ clock }) => {
+    sphereGroup.current.rotation.x += 0.003
+  })
+  return (
+    <>
+      <ambientLight intensity={1} />
+      <directionalLight />
+      <group position={[-8, -2, 0]} ref={sphereGroup}>
+        <mesh>
+          <sphereGeometry args={[4, 50, 50]} />
+          <meshStandardMaterial map={paintMap} />
+        </mesh>
+        <mesh>
+          <sphereGeometry args={[4, 50, 50]} />
+          <meshStandardMaterial map={blurMap} transparent={true} opacity={1} color={mode === 'light' ? 0xffffff : 0x000000} />
+        </mesh>
+      </group>
+    </>
+  )
+}
+
+function Background() {
+
+  return (
+    <>
+      <div id="backgroundAnim">
+        <Canvas>
+          <Suspense fallback={null}>
+            <Scene />
+          </Suspense>
+        </Canvas>
+      </div>
+    </>
+  )
+}
+
+export default Background
--- src/components/TemplateContainer.tsx
+++ src/components/TemplateContainer.tsx
@@ -5,6 +5,7 @@ import Container from '@mui/material/Container'
 import React, { useContext } from 'react'
 import MainMenu from './MainMenu'
 import Footer from './Footer'
+import Background from './Background'
 
 export default function TemplateContainer({ children }: { children: React.ReactNode }) {
     const {mode} = useContext(ThemeContext)
@@ -12,6 +13,7 @@ export default function TemplateContainer({ children }: { children: React.ReactN
         <>
             <div id="page" className={mode}>
                 <MainMenu />
+                <Background />
                 <div id="glassContainer"></div>
                 <div id='frame' className={mode}></div>
                 <div id="pageContent" className={mode}>
